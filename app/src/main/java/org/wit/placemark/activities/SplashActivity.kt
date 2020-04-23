f0f2e45eb06cce8b2a0f6744fe56b4d614965165
Kyle Fox <Kylexz@icloud.com>
2020-04-23 19:54:07 +0100

    Fix for splash screen!

--- app/src/main/java/org/wit/placemark/activities/SplashActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/SplashActivity.kt
@@ -21,20 +21,10 @@ class SplashActivity : AppCompatActivity() {
         //4second splash time
         Handler().postDelayed({
             //start main activity
-            startActivity(Intent(this@SplashActivity, PlacemarkActivity::class.java))
+            startActivity(Intent(this@SplashActivity, PlacemarkListActivity::class.java))
             //finish this activity
             finish()
         },4000)
 
     }
 }
-
-/*
-<activity android:name=".activities.SplashActivity"
-                    android:theme="@style/Theme.AppCompat.Light.NoActionBar">
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN" />
-                <category android:name="android.intent.category.LAUNCHER" />
-            </intent-filter>
-        </activity>
- */
